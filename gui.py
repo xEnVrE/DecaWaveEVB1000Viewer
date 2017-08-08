295edde2a1bcbdc67bb01988ead5a224edcb63e9
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-08-08 15:24:03 +0200

    implement Logger class

--- gui.py
+++ gui.py
@@ -5,6 +5,9 @@ from matplotlib.figure import Figure
 # neded for static canvas (example)
 from numpy import arange, sin, pi
 
+# needed for logger
+from time import localtime, strftime
+
 from PyQt5 import QtWidgets
 
 from evb1000viewer import Ui_MainWindow
@@ -19,7 +22,7 @@ class MplCanvas(FigureCanvas):
         self.axes = fig.add_subplot(111)
         self.compute_figure()
         FigureCanvas.__init__(self, fig)
-
+        
         #add parent
         self.setParent(parent)
 
@@ -57,6 +60,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.actionAbout.triggered.connect(self.about)
         self.ui.actionQuit.triggered.connect(self.quit)
 
+        # instantiate the Logger
+        self.logger = Logger(self.ui.logLabel, self.ui.logScrollArea)
+        
         # dictionary of the widgets that reppresent each Tag
         self.tags_widgets = dict()
 
@@ -75,7 +81,7 @@ class MainWindow(QtWidgets.QMainWindow):
         tag_item = tagItem()
         tag_item.ui.tagIdLabelValue.setText(tag_id)
         tag_item.ui.portLabelValue.setText(port)
-        tag_item.ui.frequencyLabelValue.setText('-')
+        tag_item.ui.frequencyLabelValue.setText("-")
 
         # add the widget to the dictionary
         self.tags_widgets[key] = tag_item
@@ -118,6 +124,46 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         self.close()
 
+class Logger:
+    """Logger class"""
+    def __init__(self, log_widget, scroll_area):
+        """
+        Handle the log viewer
+        """
+        self.log_widget = log_widget
+        self.scroll_area = scroll_area
+
+        # print in the logger at the initialization
+        txt = "Start"
+        self.log_out(txt)
+        
+    def add_tag(self, tag_name):
+        txt = tag_name + " connected"
+        self.log_out(txt)
+
+    def remove_tag(self, tag_name):
+        txt = tag_name + " removed"
+        self.log_out(txt)
+
+    def log_out(self, text):
+        """ Print string in the Log"""
+        
+        # get old text in log viewer
+        old_text = self.log_widget.text()
+
+        # add timestamp        
+        time_str = strftime(" [%H:%M:%S] ", localtime())
+
+        # print log
+        new_text = old_text + time_str + text + "\n"        
+        self.log_widget.setText(new_text)
+
+        # adjust scrollbar
+        scroll_bar = self.scroll_area.verticalScrollBar()
+        scroll_bar.setMaximum(1000)        
+        scroll_bar.setValue(scroll_bar.maximum())
+
+    
 class tagItem(QtWidgets.QWidget):
     """ tagItem class """
     def __init__(self, parent = None):
@@ -138,6 +184,16 @@ gui.add_canvas()
 # add two tags (only for example)
 gui.add_widget_tag("TAG0", "Tag 0", "COM3")
 gui.add_widget_tag("TAG1", "Tag 1", "COM2")
+
+# print in the log viewer (only for example)
+gui.logger.add_tag("Tag 0")
+gui.logger.add_tag("Tag 1")
+gui.logger.add_tag("Tag 2")
+gui.logger.remove_tag("Tag 0")
+gui.logger.add_tag("Tag 0")
+gui.logger.add_tag("Tag 3")
+gui.logger.add_tag("Tag 4")
+
 
 # show the main window
 gui.show()
