b407cac24acf5a4c08375173560b811604abfe45
xEnVrE <nicolapiga@gmail.com>
2017-08-08 12:56:22 +0200

    -change EVB1000Discovery class to DeviceManager -implement class Device which abstracts the serial port and inherits from QThread -implement Device thread creation when new device is connected -implement Device thread closing when device is disconnected

---
+++ device_manager.py
@@ -0,0 +1,225 @@
+# pyserial
+import serial
+from serial.tools import list_ports
+from serial.tools.list_ports_common import ListPortInfo
+from serial.serialutil import SerialException
+
+# make ListPortInfo hashable
+def hash_fun(self):
+    return hash(str(self))
+ListPortInfo.__hash__ = hash_fun
+
+# multi-threading
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
+from threading import Lock
+
+# sleep
+from time import sleep
+
+class Device(QThread):
+    """
+    Represents an EVB1000 Tag connected through a serial port.
+
+    Inherits from threading.Thread to handle serial i/o operations
+    in background.
+    """
+
+    def __init__(self, port):
+        # call Thread constructor
+        QThread.__init__(self)
+        
+        # save port
+        self.port = port
+
+        # configure device
+        self.configure()
+
+        # set device state
+        self._state = 'running'
+        self.state_lock = Lock()
+
+    @property
+    def state(self):
+        self.state_lock.acquire()
+        value = self._state
+        self.state_lock.release()
+        
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        self.state_lock.acquire()
+        self._state = new_state
+        self.state_lock.release()
+
+    def stop_device(self):
+        """
+        Change the state of the device from 'running' to 'stopped'.
+        """
+
+        self.state = 'stopped'
+
+    def run(self):
+        """
+        Thread main method.
+        """
+
+        if not self.connect():
+            return
+        
+        while self.state == 'running':
+            try:
+                line_byte = self.serial.readline()
+            except SerialException:
+                pass
+
+        if self.state == 'stopped':
+            self.close()
+
+        print('dead')
+        
+    def configure(self):
+        """
+        Get a serial.Serial instance and configure it.
+        """
+
+        # instantiate Serial
+        self.serial = serial.Serial()
+
+        # set port_path, e.g. '/dev/ttyUSBx' or 'COMx'
+        self.serial.port = self.port.device
+
+        # set baudrate
+        self.serial.baudrate = 115200
+
+    def connect(self):
+        """
+        Open the serial port.
+
+        return Serial.is_open
+        """
+
+        self.serial.open()
+
+        return self.serial.is_open
+
+    def close(self):
+        """
+        Close the serial port.
+
+        return Serial.is_open
+        """
+
+        self.serial.close()
+
+class DeviceManager(QThread):
+    """
+    Manage EVB1000 Tag devices connected through a serial port.
+    """
+    
+    def __init__(self, target_vid, target_pid):
+
+        # call Thread constructor
+        QThread.__init__(self)
+
+        # empty list of ports
+        self.connected_ports = []
+
+        # empty dictionary of devices
+        self.configured_devices = dict()
+
+        # store pid and vid for the target device of interest
+        self.target_vid = target_vid
+        self.target_pid = target_pid
+
+    def run(self):
+        """
+        Thread main method.
+        """
+
+        while(1):
+            try:
+                # update new and remove ports
+                new_ports, removed_ports = self.update_ports()
+
+                if new_ports:
+                    print('New ports:')
+                    print(new_ports)
+                    for p in new_ports:
+                        print(p)
+                        print(p.hwid)
+                if removed_ports:
+                    print('Removed ports:')
+                    print(removed_ports)
+
+                # configure devices connected to ports in new_ports
+                self.configure_devices(new_ports)
+                    
+                # removed devices that were disconnected
+                self.remove_devices(removed_ports)
+                
+            except KeyboardInterrupt:
+                print('Bye bye')
+                return 
+
+    def configure_devices(self, ports):
+        """
+        Configure devices connected via serial ports in ports.
+        """
+
+        # for each port create a new Device and start the underlying thread
+        for p in ports:
+            print(hash(p))
+            self.configured_devices[p] = Device(p)
+            self.configured_devices[p].start()
+
+    def remove_devices(self, ports):
+        """
+        Remove devices whose ports were removed.
+        """
+
+        # for each port change the state of the underlying thread
+        # from 'running' to 'stopped' using stop_device()
+        for p in ports:
+            print(hash(p))
+            self.configured_devices[p].stop_device()
+
+    def update_ports(self):
+        """
+        Update list of serial ports connected.
+
+        New ports are added to connected_ports if the underlying
+        usb device match the target VID and PID.
+        Missing ports are remove from connected_ports.
+
+        Return a list containing new devices.
+        Return a list containing removed devices.
+        """
+        
+        # fetch only those ports having
+        # VID:PID == target_vid:target_pid
+        vid_pid = self.target_vid + ':' + self.target_pid
+        ports = [p for p in list_ports.grep(vid_pid)]
+        
+        # add new ports to connected_ports
+        # and update new_ports
+        new_ports = []
+        for p in ports:
+            if not p in self.connected_ports:
+                self.connected_ports.append(p)
+                new_ports.append(p)
+
+        # remove missing ports from devices_found
+        # and update removed_ports
+        removed_ports = []
+        for p in self.connected_ports:
+            if not p in ports:
+                self.connected_ports.remove(p)
+                removed_ports.append(p)
+
+        return new_ports, removed_ports
+
+if __name__ == '__main__':
+    dev_man = DeviceManager(target_vid = '0403', target_pid = '6001')
+    dev_man.run()
+    
